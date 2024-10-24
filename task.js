7ea34243ef6076bf0ae337479737814c260f3074
ranjith reddy <katipallyshivanireddy1@gmail.com>
2024-10-24 12:47:39 +0530

    control statements file created

---
+++ task.js
@@ -0,0 +1,85 @@
+// for loop
+for (var a = 1; a < 3; a++) {
+    console.log(a)
+}
+
+for (var a = 1; a <= 7; a++) {
+    console.log(a)
+}
+// post decrement
+
+for (var a = 11; a > 6; a--) {
+    console.log(a)
+}
+
+for (var a = 100; a >= 90; a--) {
+    console.log(a)
+}
+// while loop
+
+var a = 1;
+while (a <= 7) {
+    console.log(a)
+    a++
+}
+
+var a = 10;
+while (a <= 20) {
+    console.log(a)
+    a++
+}
+
+var a = 1;
+while (a < 3) {
+    console.log(a)
+    a++
+}
+
+var a = 1;
+while (a < 7) {
+    console.log(a)
+    a++
+}
+// post decrement
+var a = 10;
+while (a >= 6) {
+    console.log(a)
+    a--
+}
+
+for (var i = 0; i <= 5; i++) { }
+console.log(i)
+
+var i = 1;
+while (i < 4) {
+    i++
+}
+console.log(i)
+
+// do while loop
+
+var a = 1;
+do {
+    a++
+    console.log(a)
+}
+while (a <= 5)
+
+var a = 10;
+do {
+    console.log(a)
+    a--
+} while (a >= 7)
+
+var a = 1;
+do {
+    console.log(a)
+    a++
+} while (a >= 7)
+
+var a = 10;
+do {
+    a--
+    console.log(a)
+}
+while (a >= 7)
